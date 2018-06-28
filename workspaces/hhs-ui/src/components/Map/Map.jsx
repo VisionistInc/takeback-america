f024eb69dcfe2c9d263a2fcc357ef435f53763e2
Gregory Friedman <Greg.Friedman@visionistinc.com>
2018-06-28 16:18:51 -0400

    feature: Improve performance significantly when zoomed in

--- workspaces/hhs-ui/src/components/Map/Map.jsx
+++ workspaces/hhs-ui/src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import {
   Map as LeafletMap,
   TileLayer,
@@ -10,36 +10,33 @@ import {
 } from "react-leaflet";
 import styles from "./Map.scss";
 
-const Map = ({
-  accessToken,
-  center,
-  zoom,
-  takeBackFilter,
-  counties,
-  states,
-  dropMarkers,
-  markerLatLng,
-  styleCounty,
-  styleState,
-  onCountyClick,
-  onCountyMouseOver,
-  onCountyMouseOut,
-  onZoomChange,
-  onMove,
-  setGeoJsonLayerRef,
-  zoomTakeBackFilter,
-  scoreFilter
-}) => {
+class Map extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  const onEachFeature = (feature, layer) => {
+    this.leafletMap = undefined;
+  }
+
+  componentDidMount() {
+    this.leafletMap = this.refs.map.leafletElement;    
+  }
+
+  onEachFeature = (feature, layer) => {
+    const {
+      onCountyClick,
+      onCountyMouseOver,
+      onCountyMouseOut 
+    } = this.props;
+  
     layer.on({
       click: onCountyClick,
       mouseover: onCountyMouseOver,
       mouseout: onCountyMouseOut
     });
   }
-
-  const filterFeatures = (feature) => {
+  
+  filterFeatures = (feature) => {
+    const { scoreFilter } = this.props;
     const { Overall } = feature.properties;
     if (Overall) {
       return Overall >= scoreFilter[0] && Overall <= scoreFilter[1];
@@ -47,60 +44,93 @@ const Map = ({
     return true;
   }
 
-  return (
-    <div className={styles.Map}>
-      <LeafletMap
-        center={center}
-        minZoom={3}
-        zoom={zoom}
-        zoomControl={false}
-        preferCanvas
-        onZoom={({ target }) => onZoomChange(target.getZoom())}
-        onMove={onMove}
-      >
-        <ZoomControl position="topright" />
-        <TileLayer
-          url={`https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=${accessToken}`}
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        {markerLatLng && <Marker position={markerLatLng} />}
-        {(takeBackFilter || zoomTakeBackFilter) &&
-          dropMarkers.map(({ lat, lng, name, address, googleMapsUrl }, idx) => (
-            <CircleMarker
-              key={idx}
-              fillColor="#66BBFF"
-              fillOpacity={1}
-              center={[Number(lat), Number(lng)]}
-              radius={4}
-            >
-              <Popup>
-                <div>
-                  <p>
-                    {name} <br />
-                    <a href={googleMapsUrl} target="_blank">
-                      {address}
-                    </a>
-                  </p>
-                </div>
-              </Popup>
-            </CircleMarker>
-          ))}
-        <GeoJSON
-          key={scoreFilter.toString()} //Key will allow the re-render to occur when filters are picked
-          data={counties}
-          style={styleCounty}
-          onEachFeature={onEachFeature}
-          ref={setGeoJsonLayerRef}
-          filter={filterFeatures}
-        />
-        <GeoJSON
-          data={states}
-          style={styleState}
-          interactive={false}
-        />
-      </LeafletMap>
-    </div>
-  );
-};
+  filterDropMarkers = markers => {
+    if (!this.leafletMap) return markers;
+    const mapBounds = this.leafletMap.getBounds();
+
+    return markers.filter(marker => {
+      const latLng = L.latLng([marker.lat, marker.lng]);
+      return mapBounds.contains(latLng);
+    });
+  }
+
+  render() {
+    const {
+      accessToken,
+      center,
+      zoom,
+      takeBackFilter,
+      counties,
+      states,
+      dropMarkers,
+      markerLatLng,
+      styleCounty,
+      styleState,
+      onZoomChange,
+      onMove,
+      setGeoJsonLayerRef,
+      zoomTakeBackFilter,
+      scoreFilter
+    } = this.props;
+    
+    const filteredMarkers = this.filterDropMarkers(dropMarkers);
+    
+    return (
+      <div className={styles.Map}>
+        <LeafletMap
+          center={center}
+          minZoom={3}
+          zoom={zoom}
+          zoomControl={false}
+          preferCanvas
+          onZoom={({ target }) => onZoomChange(target.getZoom())}
+          onMove={onMove}
+          ref="map"
+        >
+          <ZoomControl position="topright" />
+          <TileLayer
+            url={`https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=${accessToken}`}
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          />
+          {markerLatLng && <Marker position={markerLatLng} />}
+          {(takeBackFilter || zoomTakeBackFilter) &&
+            filteredMarkers.map(({ lat, lng, name, address, googleMapsUrl }, idx) => (
+              <CircleMarker
+                key={idx}
+                fillColor="#66BBFF"
+                fillOpacity={1}
+                center={[Number(lat), Number(lng)]}
+                radius={4}
+              >
+                <Popup>
+                  <div>
+                    <p>
+                      {name} <br />
+                      <a href={googleMapsUrl} target="_blank">
+                        {address}
+                      </a>
+                    </p>
+                  </div>
+                </Popup>
+              </CircleMarker>
+            ))}
+          <GeoJSON
+            key={scoreFilter.toString()} //Key will allow the re-render to occur when filters are picked
+            data={counties}
+            style={styleCounty}
+            onEachFeature={this.onEachFeature}
+            ref={setGeoJsonLayerRef}
+            filter={this.filterFeatures}
+          />
+          <GeoJSON
+            data={states}
+            style={styleState}
+            interactive={false}
+          />
+        </LeafletMap>
+      </div>
+    );
+  }
+}
 
 export default Map;
